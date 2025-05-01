95417b387a0ba25adff5cb595932348f903182d6
Eder <eder.tarifa@alumnos.upm.es>
2025-05-01 10:35:19 +0100

    Inicio: estructura CV web

---
+++ src/Presentation.jsx
@@ -0,0 +1,8 @@
+export default function Presentation() {
+    return (
+      <section>
+        <h1>Nombre Apellido</h1>
+        <p>Cuerpo de la carta de motivación…</p>
+      </section>
+    );
+  }

8150db2aeb0852bbb4091e278d7a22505be519b7
Eder <eder.tarifa@alumnos.upm.es>
2025-05-01 17:05:37 +0100

    Test

---
+++ src/CVInfographic.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import React from 'react';
+export default function Presentation() {
+    return (
+      <section>
+        <h1>Nombre Apellido</h1>
+        <p>Cuerpo de la carta de motivación…</p>
+      </section>
+    );
+  }
---
+++ src/VideoCV.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Presentation() {
+    return (
+      <section>
+        <h1>Nombre Apellido</h1>
+        <p>Cuerpo de la carta de motivación…</p>
+      </section>
+    );
+  }

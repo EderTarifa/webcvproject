a61f39897139ca7f00880dab7492b14767b4f026
Eder <eder.tarifa@alumnos.upm.es>
2025-05-01 17:17:24 +0100

    Updates

---
+++ precache-manifest.e0a737a52a51da689e1aa6b1ade4cd43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271b6e6f2a5da63351613a55b5e6fd63",
+    "url": "/mi-cv-web/index.html"
+  },
+  {
+    "revision": "3e8dba35ea1d755604f3",
+    "url": "/mi-cv-web/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "ec0935851c1edf4cb708",
+    "url": "/mi-cv-web/static/js/2.9e11b540.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/mi-cv-web/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "3e8dba35ea1d755604f3",
+    "url": "/mi-cv-web/static/js/main.fffdfd66.chunk.js"
+  },
+  {
+    "revision": "255470231d4b030ffbdd",
+    "url": "/mi-cv-web/static/js/runtime~main.f1fc08de.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/mi-cv-web/static/media/logo.06e73328.svg"
+  }
+]);
